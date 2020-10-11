0e130df845fed7b1670261185788609156db2212
Thakur-Govind <govind.thakur11@gmail.com>
2020-10-11 11:33:55 +0530

    GT commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#Neeed a division function
+def subtract(a,b):
+    return a-b
+def add (a,b):
+    return(a+b)
+def div(a,b):
+    if b!=0:
+        return a//b
+    else:
+        return "BHAK"
+def sqrt(a):
+    return "square root(a)"
+def mult(a,b):
+    return a*b    
+def add (a,b):
+    return(a+b)
