41a78e672eb69c507db4764d0b09195ddf8edc79
havanoor <havanoor@gmail.com>
2020-10-11 11:37:41 +0530

    Fist :tada:

---
+++ calculator.py
@@ -0,0 +1,20 @@
+#Neeed a division function
+def subtract(a,b):
+    return a-b
+def add (a,b):
+    return(a+b)
+def div(a,b):
+    if b!=0:
+        return a//b
+    else:
+        return "BHAK"
+def sqrt(a):
+    return "square root(a)"
+def mult(a,b):
+    return a*b    
+def add (a,b):
+    return(a-b)
+
+
+def helo():
+    return "hekehele"
